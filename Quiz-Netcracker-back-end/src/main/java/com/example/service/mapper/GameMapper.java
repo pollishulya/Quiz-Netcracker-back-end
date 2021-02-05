22f07a6c873447193e867efc95b83f615ea0a6d7
Aleksey <yefimav1999@gmail.com>
2021-02-06 01:19:31 +0300

    Fixed V1 и V2 (Migrations)

--- Quiz-Netcracker-back-end/src/main/java/com/example/service/mapper/GameMapper.java
+++ Quiz-Netcracker-back-end/src/main/java/com/example/service/mapper/GameMapper.java
@@ -35,7 +35,7 @@ public class GameMapper implements Mapper<Game, GameDto> {
         gameDto.setTitle(entity.getTitle());
 
         gameDto.setAverageRating(entity.getAverageRating());
-        gameDto.setGameCategory(entity.getGameCategory().getId());
+        //gameDto.setGameCategory(entity.getGameCategory().getId());
 
         gameDto.setViews(entity.getViews());
         gameDto.setRatingCount(entity.getRatingCount());
@@ -55,13 +55,13 @@ public class GameMapper implements Mapper<Game, GameDto> {
     public Game toEntity(GameDto dto) {
         Player player = playerService.findPlayerByUserId(dto.getPlayer());
         Game game = new Game();
-        GameCategory gameCategory = gameCategoryService.findGameCategoryById(dto.getGameCategory());
+        //GameCategory gameCategory = gameCategoryService.findGameCategoryById(dto.getGameCategory());
         game.setId(dto.getId());
         game.setDescription(dto.getDescription());
         game.setTitle(dto.getTitle());
 
         game.setAverageRating(dto.getAverageRating());
-        game.setGameCategory(gameCategory);
+        //game.setGameCategory(gameCategory);
         game.setViews(dto.getViews());
         game.setRatingCount(dto.getRatingCount());
 
